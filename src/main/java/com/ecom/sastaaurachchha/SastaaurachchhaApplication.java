bf4fe4964827a89ec71a49192a9c56d99d570505
Ashutosh Tiwari <ashutosh_tiwari@intuit.com>
2023-12-14 00:14:57 +0530

    first commit

---
+++ src/main/java/com/ecom/sastaaurachchha/SastaaurachchhaApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.sastaaurachchha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SastaaurachchhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SastaaurachchhaApplication.class, args);
+	}
+
+}
